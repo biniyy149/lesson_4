648f10a4a09a3b77ba8acd73f8424fd04b401be1
biniyy149 <69608146+biniyy149@users.noreply.github.com>
2020-08-29 21:21:12 -0700

    changed

--- quest8-1.js
+++ quest8-1.js
@@ -1,7 +1,13 @@
 const prompt = require('prompt-sync')();
-let num = prompt('Enter a number: ');
-num = parseInt(num);
-for(let i = 0; i < num; i++){
-    console.log('sunshine');
-    
-}
+
+let num;
+let sum = 0;
+num = parseInt(prompt('enter number: '));
+do{
+   // num = parseInt(prompt('enter number: '));
+    let digit = num%10;
+    //sum = sum * 10 + digit;
+    num = Math.floor(num/10);
+
+}while( num > 0);
+console.log(sum);
